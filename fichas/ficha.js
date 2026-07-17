af46816e35736cd40ccb62ce1be4fe3c87c5c294
marcelofveras <88948515+marcelofveras@users.noreply.github.com>
2026-07-17 15:51:00 -0300

    Ajuste URL

--- fichas/ficha.js
+++ fichas/ficha.js
@@ -23,7 +23,7 @@ async function loadClass(className, element) {
 	try {
 
 		const response = await fetch(
-			'/api/ficha.php?turma=' + encodeURIComponent(className)
+			'api/ficha.php?turma=' + encodeURIComponent(className)
 		);
 
 		const students = await response.json();
