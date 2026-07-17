db6a5bad7382cf8c3660b18d6d1ee219ccbc34da
marcelofveras <88948515+marcelofveras@users.noreply.github.com>
2026-07-17 15:48:18 -0300

    Ajuste URL API

--- lanches/lanches.js
+++ lanches/lanches.js
@@ -39,7 +39,7 @@ async function loadPeriodo(
     try {
 
 const response = await fetch(
-    '../api/lanches.php?periodo='
+    'lanches/api/lanches.php?periodo='
     + encodeURIComponent(periodo)
 );
 
